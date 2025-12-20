e40c6ecaec69ca2551cabf2e4606f221a9b27cbb
Muhammad Osama <osamabinaslam0@gmail.com>
2025-12-21 00:23:15 +0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (89.38%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -2,11 +2,12 @@ class Solution(object):
     def removeDuplicates(self, nums):
         if not nums:
             return 0
-        k = 1
-
-        for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[k] = nums[i]
+        k = 0
+        prev = None
+        for i in nums:
+            if i != prev:
+                nums[k] = i
+                prev = i
                 k += 1
         
         return k        
