50d4e7876445e87016c1dcce6663f0908331fc37
Muhammad Osama <osamabinaslam0@gmail.com>
2025-12-21 05:22:02 +0500

    Time: 3 ms (61.41%) | Memory: 13.8 MB (60.00%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -2,14 +2,19 @@ class Solution(object):
     def removeDuplicates(self, nums):
         if not nums:
             return 0
-        k = 0
-        prev = None
-        for i in nums:
-            if i != prev:
-                nums[k] = i
-                prev = i
+        
+        # 'k' is the index where the next unique element goes
+        k = 1
+        
+        # We start iterating from the second element
+        for i in range(1, len(nums)):
+            # If current element is different from previous
+            if nums[i] != nums[i - 1]:
+                # Move the unique element to the 'k' position
+                nums[k] = nums[i]
                 k += 1
-        
-        return k        
+                
+        return k
 
-        
+# google gemini3 pro solution
+#https://gemini.google.com/share/053fdc84bf14
